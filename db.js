e15a492aee085231f2bbebec03e4e04f71a0a7af
Manoj bhardwaj <53604348+manojsuthar1412@users.noreply.github.com>
2023-01-14 16:34:15 +0530

    updated mongo connect uri

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
-const keys = require('./keys')
 
-mongoose.connect(keys.mongoUrl,  { useNewUrlParser: true ,useUnifiedTopology: true }, (err) => {
-    if(!err) {
-        console.log('MongoDB connection succeded');
-    } else {
-        console.log('Error in DB connection:- ' + err);
-        
-    }
-});
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log(`ERROR: ${err}`));
+
 
 module.exports = { mongoose };
